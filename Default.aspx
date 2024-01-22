867b5fd15892e704a1cf81b159031e8dd346bd0d
DaniloTrobica <torbicad06@gmail.com>
2024-01-22 17:33:22 +0100

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication11._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+
+        Prikaz sedišta autobusa<br />
+        <asp:Table ID="Table1" runat="server">
+        </asp:Table>
+        <br />
+        <table style="width:100%;">
+            <tr>
+                <td style="width: 164px">Broj sedišta</td>
+                <td>
+                    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td style="width: 164px">Ime i prezime</td>
+                <td>
+                    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td style="width: 164px">E-mail</td>
+                <td>
+                    <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+        <asp:Button ID="Button1" runat="server" Text="Pošalji" Height="21px" Width="50px" />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+
+        <asp:ListBox ID="ListBox1" runat="server"></asp:ListBox>
+
+    </div>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace WebApplication11
+{
+    public partial class _Default : Page
+    {
+        public string conStr = "Data Source=.\\SQLEXPRESS;Initial Catalog=rezervacije;Integrated Security=True";
+        public const int min_sedista = 2;
+        public const int max_sedista = 53;
+        public List<int> rezervisana = new List<int>();
+        private List<Button> mesta = new List<Button>();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public bool rezervisano(int sedište)
+        {
+            foreach (int broj in rezervisana)
+            {
+                if (broj == sedište)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void iscitaj()
+        {
+            string select = "select brojSedista from sedista";
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = conStr;
+            SqlCommand cmd = new SqlCommand(select, con);
+            SqlDataReader reader;
+            using(con)
+            {
+                try
+                {
+                    con.Open();
+                    reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        rezervisana.Add(Int32.Parse(reader["brojSedista"].ToString()));
+                    }
+                    reader.Close();
+                    con.Close();
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+        private void kreirajMesta()
+        {
+            for (int i = min_sedista; i<=max_sedista;++i)
+            {
+                Button mesto = new Button();
+                mesto.Text = i + "";
+                mesta.Add(mesto);
+            }
+        }
+        private void izaberiMesto(object o, EventArgs e)
+        {
+            Button b = (Button)o;
+            txtSediste.Text = b.Text;
+        }
+        public void kreirajTabelu()
+        {
+
+        }
+    }
+}
